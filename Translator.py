59b314dad980bf3e3d5b7abdfab0a783df39cfac
Tomáš Bartoš <128893531+BartosTomas@users.noreply.github.com>
2023-05-20 08:12:53 +0200

    edit Translator

--- Translator.py
+++ Translator.py
@@ -1,23 +1,23 @@
 class Translator:
     def __init__(self):
+
         self.transcript = []
         self.width
         self.height
-        
-        self.script
-        self.dictionary
+        #self.dictionary use if not just 0's 1's and 2
         self.translate()
         self.dimensions()
 
     def translate(self):
         while True:
-            line = []
-            with open("C:\Users\42077\OneDrive\GitHub.me\Maze_\MazeScript1") as vstup:
-                self.script = vstup.readline()
-                if self.script != "":
-                    for number in self.script.sprlit(","):
-                        line.append(int(number))
-                    self.transcript.append(line)
+            cut = []
+            file = open("C:\Users\42077\OneDrive\GitHub.me\Maze_\MazeScript1")
+            script = file.readlines()
+            for line in script:
+                if line != "":
+                    for number in line.split(","):
+                        cut.append(int(number))
+                    self.transcript.append(cut)
                 else:
                     break
 
