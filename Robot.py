42135531bb9e038d71cb45e2ed2e312d02ce259c
Tomáš Bartoš <128893531+BartosTomas@users.noreply.github.com>
2023-05-21 09:33:25 +0200

    Robot file

---
+++ Robot.py
@@ -0,0 +1,12 @@
+# this is a file for robot
+
+class Robot:
+    def __init__(self):
+        path = 1
+
+    def navigation(self,path):
+        path = 1
+
+    def sprite(self):
+        # draw sprite
+        sprite = 1
