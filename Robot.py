9c9f01252a2775f622999d378080978bc572480a
Tomáš Bartoš <128893531+BartosTomas@users.noreply.github.com>
2023-05-21 18:37:16 +0200

    Robot edit - possible routs

--- Robot.py
+++ Robot.py
@@ -1,12 +1,49 @@
-# this is a file for robot
+from Translator import *
 
 class Robot:
     def __init__(self):
-        path = 1
+        self.translator = Translator()
+        translator.translate(r"C:\Users\42077\OneDrive\GitHub.me\Maze_\MazeScript2")
+        self.transcript = self.translator.transcript
+        self.xy = [0,0]
+        self.check_surroundings()
+        
+    def check_surroundings(self):
+        x = self.xy[0]
+        y = self.xy[1]
 
-    def navigation(self,path):
-        path = 1
+        possible_routs = []
+        # S
+        try:
+            if self.transcript[y+1][x] != "1":
+                possible_routs.append(self.transcript[y+1][x])
+        except IndexError:
+            pass
+        # N
+        try:
+            if self.transcript[y-1][x] != "1":
+                possible_routs.append(self.transcript[y-1][x])
+        except IndexError:
+            pass
+        
+        # E
+        try:
+            if self.transcript[y][x-1] != "1":
+                possible_routs.append(self.transcript[y][x-1])
+        except IndexError:
+            pass
+        
+        # W
+        try:
+            if self.transcript[y][x+1] != "1":
+                possible_routs.append(self.transcript[y][x+1])
+        except IndexError:
+            pass
+        
+        print(possible_routs)
+    
+    def navigation(self):
+        pass
 
     def sprite(self):
-        # draw sprite
-        sprite = 1
+        pass
